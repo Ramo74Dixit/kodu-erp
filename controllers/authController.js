3a7436d5ba7b2620ef85338c4aba5a96f198f691
Ramo74Dixit <ramodixit76@gmail.com>
2025-02-23 19:18:16 +0530

    ghjbknml,./

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,18 +2,25 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const { JWT_SECRET } = require('../config/config');
+
 const loginUser = async (req, res) => {
     const { email, password } = req.body;
-    const user = await User.findOne({ email });
+
+    // Find user and index the email field for optimization
+    const user = await User.findOne({ email }).lean();
     if (!user) {
         return res.status(400).json({ message: 'Invalid credentials' });
     }
+
+    // Compare password
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
         return res.status(400).json({ message: 'Invalid credentials' });
     }
-    const token = jwt.sign({ userId: user._id, role: user.role }, JWT_SECRET, { expiresIn: '1h' });
-    console.log(token)
+
+    // JWT Generation - Async
+    const token = await jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: '1h' });
+
     res.json({
         message: 'Login successful',
         token
